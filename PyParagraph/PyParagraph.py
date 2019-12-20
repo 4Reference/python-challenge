e8a05d812d3a66647c5c5bbfa998011a61633eac
Subthermal <53093356+Subthermal@users.noreply.github.com>
2019-12-19 19:04:50 -0600

    victory

--- PyParagraph/PyParagraph.py
+++ PyParagraph/PyParagraph.py
@@ -1,37 +1,35 @@
 import os
+import re
 
 wordlist=[]
 sentlist=[]
 
-# inputtext = input("Please enter the text you would like analyzed: ")
-# sentlist = inputtext.split(".")
-# print(sentlist)
-# print(len(sentlist))
-
-
-# with open("PyParaAnalyze.txt",'w+') as file:
-#     file.write(inputtext)
-# using pre-canned text
 txtfile = os.path.join(".","raw_data","paragraph_2.txt")
 
 with open(txtfile, 'r') as file:
     for line in file:
+        line = line.rstrip()
         wordlist += line.split()
-        sentlist += line.split(".")
+        sentlist += line.split(". ")
+        while("" in sentlist) : 
+            sentlist.remove("") 
+
     print(sentlist)
+    wordlistlen = len(wordlist)
+    sentlistlen = len(sentlist)
     # Approximate letter count (per word)
-    avgwordlen = sum(len(word) for word in wordlist) / len(wordlist)
+    avgwordlen = round(sum(len(word) for word in wordlist) / wordlistlen,1)
     # Approximate word count per sentence
     sentlen = 0
     for i in range(0,len(sentlist)):
         templist = sentlist[i]
         sentlen = sentlen+len(templist.split())
-    avgsentlen = sentlen/len(sentlist)
+    avgsentlen = round(sentlen/sentlistlen,1)
 
 print("Paragraph Analysis")
 print("-----------------")
 print(f"Approximate Word Count: {len(wordlist)}")
 print(f"Approximate Sentence Count: {len(sentlist)}")
-print(f"Average Letter Count per word: {avgwordlen:.3}")
-print(f"Average Sentence Length: {avgsentlen:.3}")
+print(f"Average Letter Count per word: {avgwordlen}")
+print(f"Average Sentence Length: {avgsentlen}")
 print("-----------------")
